a7ac4b825c89c1450c976d7dfe6617ed979cc243
Xander Morrison <x@smlxl.co>
2019-08-21 21:54:07 -0500

    Hooking in AJAX

--- juego/static/game.js
+++ juego/static/game.js
@@ -1,17 +1,6 @@
-$(document).ready(function() {
-	let language;
-	let path = window.location.pathname;
-
-	if (path === "/en") {
-		language = "english";
-	} else if (path === "/es") {
-		language = "spanish";
-	} else if (path === "/pt") {
-		language = "portuguese";
-	} else if (path === "/jp") {
-		language = "japanese";
-	}
-
+function refreshPage(language) {
+	$("#guess").val("");
+	$("#guess").prop("disabled", false);
 	$("#guess").focus();
 
 	$.getJSON(`/verbs/${language}`, function(result) {
@@ -29,10 +18,31 @@ $(document).ready(function() {
 			$("#answer").text(result.answer);
 
 		if (language === "japanese") {
-			$(".answer-list").append(`<li class="list-group-item"><span id="kana">${result.kana}</span></li>`);
-			$(".answer-list").append(`<li class="list-group-item"><span id="romaji">${result.romaji}</span></li>`);
+			$(".jp").remove();
+			$(".answer-list").append(`<li class="list-group-item jp"><span id="kana">${result.kana}</span></li>`);
+			$(".answer-list").append(`<li class="list-group-item jp"><span id="romaji">${result.romaji}</span></li>`);
 		}
 	});
+}
+
+
+$(document).ready(function() {
+	let language;
+	let path = window.location.pathname;
+
+	console.log(window.navigator.language);
+
+	if (path === "/en") {
+		language = "english";
+	} else if (path === "/es") {
+		language = "spanish";
+	} else if (path === "/pt") {
+		language = "portuguese";
+	} else if (path === "/jp") {
+		language = "japanese";
+	}
+
+	refreshPage(language);
 
 	$("#submission").submit(function(e) {
 		e.preventDefault();
@@ -42,8 +52,12 @@ $(document).ready(function() {
 		let infinitive = $("#infinitive").text().toLowerCase();
 
 		$("#answer-box").fadeIn(500);
-		var sl = path.replace("/", "")
-		var tl = "en"
+		if (path === "/jp") {
+			var sl = "ja";
+		} else {
+			var sl = path.replace("/", "");
+		}
+		var tl = "en"; // switch out
 		$("#translate").attr("href", `https://translate.google.com/#view=home&op=translate&sl=${sl}&tl=${tl}&text=${infinitive}`);
 		if (guess === correct || (language !== "english" && ((guess === `${subject} ${correct}`) || (guess === correct)))) {
 			$(".answer").removeClass("list-group-item-danger");
@@ -65,6 +79,6 @@ $(document).ready(function() {
 	});
 
 	$("#continue").click(function(e) {
-		console.log("MADE IT HERE");
+		refreshPage(language);
 	});
 });
